60a7b9a87943fbcae59e1ef5c506a44650184804
sajithperera <sajithrumesh1998@gmail.com>
2020-07-12 13:55:44 +0530

    Correct the errors with add and remove icons in the cart page and edited the pubspec file

--- lib/Screens/awanhala.dart
+++ lib/Screens/awanhala.dart
@@ -24,7 +24,7 @@ class Awanhala extends StatelessWidget {
     return GetMaterialApp(
         title: 'Awanahala',
         debugShowCheckedModeBanner: false,
-        initialRoute: '/forgotPass',
+        initialRoute: '/cart',
         routes: {
           '/login': (context) => Login(),
           '/sighUp': (context) => SignUp(),
