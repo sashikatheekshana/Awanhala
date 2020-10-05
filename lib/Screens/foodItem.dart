fd6195c5ffc4b5ae37a883adb90933f46d0f3c67
sashikatheekshana <sashika2950@gmail.com>
2020-10-05 20:44:14 +0530

    fooditem changes

--- lib/Screens/foodItem.dart
+++ lib/Screens/foodItem.dart
@@ -1,3 +1,4 @@
+import 'package:awanahala/Screens/foodItemCategory.dart';
 import 'package:flutter/material.dart';
 
 class FoodItem extends StatefulWidget {
@@ -20,139 +21,140 @@ class _FoodItemState extends State<FoodItem> with SingleTickerProviderStateMixin
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        body: Container(
-          child: Column(
-            children: <Widget>[
-              Container(
-                decoration: BoxDecoration(
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.red[400].withOpacity(0.9),
-                      spreadRadius: 0,
-                      blurRadius: 0.5,
-                      offset: Offset(0, 0), // changes position of shadow
-                    ),
-                  ],
-                ),
-                height: 130,
-                padding: EdgeInsets.only(top: 10.0),
-                // color: Colors.red[400],
-                child: Column(
-                  children: <Widget>[
-                    Row(
-                      children: <Widget>[
-                        IconButton(
-                          icon: Icon(
-                            Icons.arrow_back,
-                            color: Colors.black87,
-                            size: 30.0,
+        body: SingleChildScrollView(
+          child: Container(
+            child: Column(
+              children: <Widget>[
+                Container(
+                  decoration: BoxDecoration(
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.red[400].withOpacity(0.9),
+                        spreadRadius: 0,
+                        blurRadius: 0.5,
+                        offset: Offset(0, 0), // changes position of shadow
+                      ),
+                    ],
+                  ),
+                  height: 130,
+                  padding: EdgeInsets.only(top: 10.0),
+                  child: Column(
+                    children: <Widget>[
+                      Row(
+                        children: <Widget>[
+                          IconButton(
+                            icon: Icon(
+                              Icons.arrow_back,
+                              color: Colors.black87,
+                              size: 30.0,
+                            ),
+                            onPressed: () {
+                              Navigator.of(context).pop();
+                            },
                           ),
-                          onPressed: () {
-                            Navigator.of(context).pop();
-                          },
-                        ),
-                        SizedBox(width: 20.0),
-                        CircleAvatar(
-                          radius: 37,
-                          backgroundColor: Colors.black,
-                          child: CircleAvatar(
-                            radius: 35.0,
-                            backgroundImage: AssetImage("images/ucsc.jpg"),
-                          ),
-                        ),
-                        SizedBox(width: 15.0),
-                        Container(
-                          padding: EdgeInsets.only(left: 20.0),
-                          width: 180.0,
-                          child: Text(
-                            "Managment Canteen ",
-                            style: TextStyle(
-                              fontSize: 20.0,
-                              fontWeight: FontWeight.w400,
-                              color: Colors.white,
+                          SizedBox(width: 20.0),
+                          CircleAvatar(
+                            radius: 37,
+                            backgroundColor: Colors.black,
+                            child: CircleAvatar(
+                              radius: 35.0,
+                              backgroundImage: AssetImage("images/ucsc.jpg"),
                             ),
                           ),
-                        ),
-                      ],
-                    ),
-                    SizedBox(height: 10.0),
-                    Container(
-                      height: 36.0,
-                      child: TabBar(
-                        unselectedLabelColor: Colors.black,
-                        labelColor: Colors.white,
-                        indicatorColor: Colors.white,
-                        indicatorWeight: 3.0,
-                        controller: _tabController,
-                        isScrollable: true,
-                        labelPadding: EdgeInsets.symmetric(horizontal: 20.0),
-                        tabs: [
-                          Tab(
+                          SizedBox(width: 15.0),
+                          Container(
+                            padding: EdgeInsets.only(left: 20.0),
+                            width: 180.0,
                             child: Text(
-                              "Breakfirst",
+                              "Managment Canteen ",
                               style: TextStyle(
-                                fontSize: 16.0,
+                                fontSize: 20.0,
                                 fontWeight: FontWeight.w400,
-                              ),
-                            ),
-                          ),
-                          Tab(
-                            child: Text(
-                              "Lunch",
-                              style: TextStyle(
-                                fontSize: 16.0,
-                                fontWeight: FontWeight.w400,
-                              ),
-                            ),
-                          ),
-                          Tab(
-                            child: Text(
-                              "Short Eats",
-                              style: TextStyle(
-                                fontSize: 16.0,
-                                fontWeight: FontWeight.w400,
-                              ),
-                            ),
-                          ),
-                          Tab(
-                            child: Text(
-                              "Juices",
-                              style: TextStyle(
-                                fontSize: 16.0,
-                                fontWeight: FontWeight.w400,
-                              ),
-                            ),
-                          ),
-                          Tab(
-                            child: Text(
-                              "Soft Drinks",
-                              style: TextStyle(
-                                fontSize: 16.0,
-                                fontWeight: FontWeight.w400,
+                                color: Colors.white,
                               ),
                             ),
                           ),
                         ],
                       ),
-                    ),
-                  ],
+                      SizedBox(height: 10.0),
+                      Container(
+                        height: 36.0,
+                        child: TabBar(
+                          unselectedLabelColor: Colors.black,
+                          labelColor: Colors.white,
+                          indicatorColor: Colors.white,
+                          indicatorWeight: 3.0,
+                          controller: _tabController,
+                          isScrollable: true,
+                          labelPadding: EdgeInsets.symmetric(horizontal: 20.0),
+                          tabs: [
+                            Tab(
+                              child: Text(
+                                "Breakfirst",
+                                style: TextStyle(
+                                  fontSize: 16.0,
+                                  fontWeight: FontWeight.w400,
+                                ),
+                              ),
+                            ),
+                            Tab(
+                              child: Text(
+                                "Lunch",
+                                style: TextStyle(
+                                  fontSize: 16.0,
+                                  fontWeight: FontWeight.w400,
+                                ),
+                              ),
+                            ),
+                            Tab(
+                              child: Text(
+                                "Short Eats",
+                                style: TextStyle(
+                                  fontSize: 16.0,
+                                  fontWeight: FontWeight.w400,
+                                ),
+                              ),
+                            ),
+                            Tab(
+                              child: Text(
+                                "Juices",
+                                style: TextStyle(
+                                  fontSize: 16.0,
+                                  fontWeight: FontWeight.w400,
+                                ),
+                              ),
+                            ),
+                            Tab(
+                              child: Text(
+                                "Soft Drinks",
+                                style: TextStyle(
+                                  fontSize: 16.0,
+                                  fontWeight: FontWeight.w400,
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
-              ),
-              SizedBox(height: 10.0),
-              Container(
-                height: 50.0,
-                child: TabBarView(
-                  controller: _tabController,
-                  children: [
-                    new Text(""),
-                    new Text(""),
-                    new Text(""),
-                    new Text(""),
-                    new Text(""),
-                  ],
+                // SizedBox(height: 10.0),
+                Container(
+                  height: 485.0,
+                  child: TabBarView(
+                    controller: _tabController,
+                    children: [
+                      FoodItemCategory("Breakfirst"),
+                      FoodItemCategory("Lunch"),
+                      FoodItemCategory("Short Eats"),
+                      FoodItemCategory("juices"),
+                      FoodItemCategory("Soft Drinks"),
+                    ],
+                  ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
